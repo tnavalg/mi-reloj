b05f50e22c05f46d57385c597677e8265e19104b
tnavalg <71338483+tnavalg@users.noreply.github.com>
2025-06-27 16:15:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
+const CACHE_NAME = 'reloj-cache-v2'; // Incrementar la versión con cada actualización
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon.png',
+  './beep.mp3'
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('reloj-cache').then(function(cache) {
-      return cache.addAll([
-        './index.html',
-        './manifest.json',
-        './sw.js',
-        './icon.png',
-        './beep.mp3'
-      ]);
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
     })
   );
 });
@@ -16,6 +20,21 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME && cacheName.startsWith('reloj-cache')) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
