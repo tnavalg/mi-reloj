897e5afb4b23668472cf6568c468b3bfaf84d5bc
tnavalg <71338483+tnavalg@users.noreply.github.com>
2025-06-30 12:11:21 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  const CACHE_NAME = 'reloj-cache-v5';
+  const urlsToCache = [
+    './index.html',
+    './manifest.json',
+    './sw.js',
+    './icon.png',
+    './beep.mp3'
+  ];
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const CACHE_NAME = 'reloj-cache-v5';
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME && cacheName.startsWith('reloj-cache')) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
